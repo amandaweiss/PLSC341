5b8e2a842247a137c8d607a644b2d9759632d275
amandaweiss <51291089+amandaweiss@users.noreply.github.com>
2023-02-20 14:43:18 -0500

    Add files via upload

---
+++ PLSC341 - Section 5.R
@@ -0,0 +1,89 @@
+################################################################################
+#                     Logic of Randomized Experiments                          #
+#                         Discussion Section #5                                #
+################################################################################
+
+# The goal of this section will be to get you familiar with the differences
+# between the Horvitz-Thompson and Hajek estimators. These exercises will also
+# get you in the habit of writing for-loops for simulations and thinking about
+# data-generating processes.
+
+# ------------------------------ Exercise 1 ------------------------------------
+# For this exercise, you should rewrite the code from Prof. Aronow's ht-sim.R
+# example file to include a calculation for the Hajek estimator, as well as the
+# Horvitz-Thompson estimator. I have copied over the code for you; your task is
+# just to modify it and consider whether the result is unbiased. So you're going
+# to want to have 6 items: the original mean, the mean of the simulated
+# Horvitz-Thompson estimates, the bias of the Horvitz-Thompson estimator in this
+# case, the mean of the simulated Hajek estimates, and the bias of the Hajek
+# estimate.
+
+
+set.seed(123)
+
+# Simulation for HT estimator
+
+N <- 5
+y <- c(4, 3, 2, 1, 4)
+mean(y) # mu
+
+# Sampling design:
+drawsample <- function() {
+  R <- rep(NA, N)
+  R[1] <- rbinom(1, 1, .5)
+  if(R[1] == 1) R[2:N] <- rbinom(N - 1, 1, .25)
+  if(R[1] == 0) R[2:N] <- rbinom(N - 1, 1, .50)
+  return(R)
+}
+
+# This is a weird sampling design. if the first unit is selected, every other
+# unit has a 25% chance of being selected. if the first unit is not selected,
+# every other unit has a 50% chance of being selected.
+
+# The probability of unit 1 being selected is 0.5. the probability of every
+# other unit is therefore .375. This vector of probabilities is formalized 
+# below.
+
+pis <- c(.5,rep(.375, N - 1))
+
+### show unbiasedness
+
+niter <- 50000
+
+htests <- rep(NA, niter)
+
+for (iter in 1:niter) {
+  R <- drawsample()
+  yR <- y*R
+  htests[iter] <- sum(yR/pis)/N
+}
+
+# ORIGINAL MEAN (mu):
+mean(y) 
+
+# MEAN OF THE SIMULATED HT ESTIMATES:
+mean(htests) 
+
+# BIAS CALCULATION FOR THE HT ESTIMATES:
+
+# MEAN OF THE SIMULATED HAJEK ESTIMATES:
+
+# BIAS CALCULATION FOR THE HT ESTIMATES:
+
+
+# ------------------------------ Exercise 2 ------------------------------------
+# For this second exercise, you will once again be calculating Horvitz-Thompson
+# and Hajek estimates, checking the biases. However, you will create your own data-generating
+# process. So, you can create your own `N`, `y` `drawsample()`, or `pis`. You
+# can base it on data-generating process that Prof. Aronow created above, but
+# you should make at least one modification.
+
+
+# --------------------------- Bonus: Exercise 3 --------------------------------
+# Write a variance function for the Horvitz-Thompson estimator for the case that
+# Prof. Aronow mentioned where there is a closed-form solution. Also compare it
+# to a bootstrap variance.
+
+
+
+
